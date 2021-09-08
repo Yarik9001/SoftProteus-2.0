877d17dbfe8b9b96c0ae93cc419457e2f3dee22c
Yarik9001 <lynx9001@yandex.ru>
2021-09-08 22:52:18 +0300

    print

--- esp/test-uart.py
+++ esp/test-uart.py
@@ -2,4 +2,5 @@ from machine import UART
 
 uart1 = UART(2, baudrate=9600, tx=17, rx=16)
 uart1.write('hello')  # write 5 bytes
-uart1.read(5)         # read up to 5 bytes
+data = uart1.read(5)         # read up to 5 bytes
+print(data)
