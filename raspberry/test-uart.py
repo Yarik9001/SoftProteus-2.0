7db8e921e10d5033b08b4e911cbc15a2f1c64cb2
Yarik9001 <lynx9001@yandex.ru>
2021-09-08 22:49:28 +0300

    ras

--- raspberry/test-uart.py
+++ raspberry/test-uart.py
@@ -1,6 +1,8 @@
 import serial
 ser = serial.Serial ("/dev/ttyAMA0")    #Open named port 
 ser.baudrate = 9600                     #Set baud rate to 9600
-data = ser.read(10)                     #Read ten characters from serial port to data
-ser.write(data)                         #Send back the received data
-ser.close()    
+data = ser.read(10)  
+#Read ten characters from serial port to data
+while True:
+    ser.write('12345')  
+    print('12345')  
