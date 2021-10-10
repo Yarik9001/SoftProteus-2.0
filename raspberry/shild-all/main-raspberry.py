6167a520eb812729b48060d5aba2b492771f043c
linux <eyenot2@yandex.ru>
2021-10-10 15:51:59 +0300

    logging

--- raspberry/shild-all/main-raspberry.py
+++ raspberry/shild-all/main-raspberry.py
@@ -1,6 +1,8 @@
 import socket  # библиотека для связи
 import board
 import busio
+import logging
+import coloredlogs
 from time import sleep  # библиотека длязадержек
 from datetime import datetime  # получение текущего времени
 from configparser import ConfigParser  # чтание конфигов
@@ -13,11 +15,52 @@ from math import atan2, pi
 import ms5837
 
 
+class MedaLogging:
+    def __init__(self):
+        self.mylogs = logging.getLogger(__name__)
+        self.mylogs.setLevel(logging.DEBUG)
+        # обработчик записи в лог-файл
+        self.file = logging.FileHandler("Main.log")
+        self.fileformat = logging.Formatter(
+            "%(asctime)s:%(levelname)s:%(message)s")
+        self.file.setLevel(logging.DEBUG)
+        self.file.setFormatter(self.fileformat)
+        # обработчик вывода в консоль лог файла
+        self.stream = logging.StreamHandler()
+        self.streamformat = logging.Formatter(
+            "%(levelname)s:%(module)s:%(message)s")
+        self.stream.setLevel(logging.DEBUG)
+        self.stream.setFormatter(self.streamformat)
+        # инициализация обработчиков
+        self.mylogs.addHandler(self.file)
+        self.mylogs.addHandler(self.stream)
+        coloredlogs.install(level=logging.DEBUG, logger=self.mylogs,
+                            fmt='%(asctime)s [%(levelname)s] - %(message)s')
+
+        self.mylogs.info('start-logging')
+
+    def debug(self, message):
+        self.mylogs.debug(message)
+
+    def info(self, message):
+        self.mylogs.info(message)
+
+    def warning(self, message):
+        self.mylogs.warning(message)
+
+    def critical(self, message):
+        self.mylogs.critical(message)
+
+    def error(self, message):
+        self.mylogs.error(message)
+
+
 class ROVProteusClient:
     #Класс ответсвенный за связь с постом 
-    def __init__(self):
+    def __init__(self, logger:MedaLogging):
+        self.logger = logger
         self.HOST = '192.168.1.100'
-        self.PORT = 1241
+        self.PORT = 1253
         self.telemetria = True
         self.checkConnect = True      
         # Настройки клиента 
@@ -28,6 +71,7 @@ class ROVProteusClient:
         #Функция для  отправки пакетов на пульт 
         if self.checkConnect:
             data['time'] = str(datetime.now())
+            self.logger.debug(str(data))
             DataOutput = str(data).encode("utf-8")
             self.client.send(DataOutput)
 
@@ -37,74 +81,98 @@ class ROVProteusClient:
             data = self.client.recv(512).decode('utf-8')
             if len(data) == 0:
                 self.checkConnect = False
+                self.logger.info('Rov-disconect')
                 self.client.close()
                 return None
             data = dict(literal_eval(str(data)))
-            if self.telemetria:
-                print(data)
+            self.logger.debug(str(data))
             return data
 
-class Acp:
-    def __init__(self):
-        '''
-        Класс описывающий взаимодействие и опрос датчиков тока 
-        '''
-        self.i2c = busio.I2C(board.SCL, board.SDA)
-        self.ads13 = ADS.ADS1115(self.i2c)
-        self.adc46 = ADS.ADS1115(self.i2c, address=0x49)
-        a1 = AnalogIn(self.ads13, ADS.P0)
-        a2 = AnalogIn(self.ads13, ADS.P1)
-        a3 = AnalogIn(self.ads13, ADS.P2)
-        a4 = AnalogIn(self.adc46, ADS.P0)
-        a5 = AnalogIn(self.adc46, ADS.P1)
-        a6 = AnalogIn(self.adc46, ADS.P2)
 
-        self.CorNulA1 = a1.value
-        self.CorNulA2 = a2.value
-        self.CorNulA3 = a3.value
-        self.CorNulA4 = a4.value
-        self.CorNulA5 = a5.value
-        self.CorNulA6 = a6.value
+class Acp:
+    def __init__(self, logger: MedaLogging):
+        '''
+        Класс описывающий взаимодействие и опрос датчиков тока
+        '''
+        self.logger = logger
+        try:
+            self.i2c = busio.I2C(board.SCL, board.SDA)
+            self.ads13 = ADS.ADS1115(self.i2c)
+            self.adc46 = ADS.ADS1115(self.i2c, address=0x49)
+            a1 = AnalogIn(self.ads13, ADS.P0)
+            a2 = AnalogIn(self.ads13, ADS.P1)
+            a3 = AnalogIn(self.ads13, ADS.P2)
+            a4 = AnalogIn(self.adc46, ADS.P0)
+            a5 = AnalogIn(self.adc46, ADS.P1)
+            a6 = AnalogIn(self.adc46, ADS.P2)
+
+            self.CorNulA1 = a1.value
+            self.CorNulA2 = a2.value
+            self.CorNulA3 = a3.value
+            self.CorNulA4 = a4.value
+            self.CorNulA5 = a5.value
+            self.CorNulA6 = a6.value
+            self.logger.info('ADC1115-init')
+        except:
+            self.logger.critical('NO-ADC1115')
         
         self.MassOut = {}
 
     def ReqestAmper(self):
-        #Функция опроса датчиков тока 
-        a1 = AnalogIn(self.ads13, ADS.P0)
-        a2 = AnalogIn(self.ads13, ADS.P1)
-        a3 = AnalogIn(self.ads13, ADS.P2)
-        a4 = AnalogIn(self.adc46, ADS.P0)
-        a5 = AnalogIn(self.adc46, ADS.P1)
-        a6 = AnalogIn(self.adc46, ADS.P2)
-        # TODO  матан для перевода значений - отсылается уже в амперах
-        self.MassOut['a0'] = round(
-            (a1.value - self.CorNulA1) * 0.00057321919, 3)
-        self.MassOut['a1'] = round(
-            (a2.value - self.CorNulA2) * 0.00057321919, 3)
-        self.MassOut['a2'] = round(
-            (a3.value - self.CorNulA3) * 0.00057321919, 3)
-        self.MassOut['a3'] = round(
-            (a4.value - self.CorNulA4) * 0.00057321919, 3)
-        self.MassOut['a4'] = round(
-            (a5.value - self.CorNulA5) * 0.00057321919, 3)
-        self.MassOut['a5'] = round(
-            (a6.value - self.CorNulA6) * 0.00057321919, 3)
-        # возвращает словарь с значениями амрепметра нумерация с нуля
-        return self.MassOut
+        try:
+            #Функция опроса датчиков тока 
+            a1 = AnalogIn(self.ads13, ADS.P0)
+            a2 = AnalogIn(self.ads13, ADS.P1)
+            a3 = AnalogIn(self.ads13, ADS.P2)
+            a4 = AnalogIn(self.adc46, ADS.P0)
+            a5 = AnalogIn(self.adc46, ADS.P1)
+            a6 = AnalogIn(self.adc46, ADS.P2)
+            # TODO  матан для перевода значений - отсылается уже в амперах
+            self.MassOut['a0'] = round(
+                (a1.value - self.CorNulA1) * 0.00057321919, 3)
+            self.MassOut['a1'] = round(
+                (a2.value - self.CorNulA2) * 0.00057321919, 3)
+            self.MassOut['a2'] = round(
+                (a3.value - self.CorNulA3) * 0.00057321919, 3)
+            self.MassOut['a3'] = round(
+                (a4.value - self.CorNulA4) * 0.00057321919, 3)
+            self.MassOut['a4'] = round(
+                (a5.value - self.CorNulA5) * 0.00057321919, 3)
+            self.MassOut['a5'] = round(
+                (a6.value - self.CorNulA6) * 0.00057321919, 3)
+            # возвращает словарь с значениями амрепметра нумерация с нуля
+            return self.MassOut
+        except:
+            self.logger.critical('NO-ADC1115')
+            return None
+
 
 class Compass:
     # класс описывающий общение с модулем навигации mpu9250
-    def __init__(self):
-        self.mpu9250 = FaBo9Axis_MPU9250.MPU9250()
+    def __init__(self, logger:MedaLogging):
+        self.logger = logger
+        
+        try:
+            self.mpu9250 = FaBo9Axis_MPU9250.MPU9250()
+            self.logger.info('MPU9250-init')
+        except:
+            self.logger.critical('NO-MPU9250')
     
     def reqiest(self):
-        # возвращает словарь с значениями азимута 
-        mag = self.mpu9250.readMagnet()
-        return {'azim':(round((atan2(mag['x'], mag['y']) * 180 / pi), 3))}
+        # возвращает словарь с значениями азимута
+        try:
+            mag = self.mpu9250.readMagnet()
+            return {'azim':(round((atan2(mag['x'], mag['y']) * 180 / pi), 3))}
+        except:
+            self.logger.critical('NO-MPU9250')
+            return None
+
 
 class DeptAndTemp:
     # класс описывающий общение с датчиком глубины 
-    def __init__(self):
+    def __init__(self, logger: MedaLogging):
+        
+        self.logger = logger
         # плотность воды 
         density = 1000 
         # илициализация сенсора 
@@ -112,6 +180,9 @@ class DeptAndTemp:
         if self.sensor.init():
             self.sensor.setFluidDensity(ms5837.DENSITY_SALTWATER)
             self.sensor.setFluidDensity(density)
+            self.logger.info('DEPT-SENSOR-init')
+        else:
+            self.logger.critical('NO-SENSOR-DEPT')
 
 
     def reqiest(self):
@@ -122,10 +193,13 @@ class DeptAndTemp:
             massout['term'] = self.sensor.temperature()
             return massout
         else:
+            self.logger.critical('NO-SENSOR-DEPT')
             return {'dept':-100,'temp': -100}
 
+
 class PwmControl:
-    def __init__(self):
+    def __init__(self,logger:MedaLogging):
+        self.logger = logger
         # диапазон шим модуляции 
         self.pwmMin = 1100
         self.pwmMax = 1950
@@ -137,7 +211,11 @@ class PwmControl:
         self.CorDrk4 = 1
         self.CorDrk5 = 1
         # инициализация платы 
-        self.kit = ServoKit(channels=16)
+        try:
+            self.kit = ServoKit(channels=16)
+            self.logger.info('PWM-CONTROL-init')
+        except:
+            self.logger.critical('NO-PWM-CONTROL')
 
         self.drk0 = self.kit.servo[0]
         self.drk0.set_pulse_width_range(self.pwmMin, self.pwmMax)
